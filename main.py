af01695127042304878a9d822781889a65ccf197
memareve <mevdishe@gmail.com>
2021-02-15 10:39:52 +0700

    beta 0.1

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
-# This is a sample Python script.
+from textblob import TextBlob
+from langdetect import detect
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+text = input('Введите текст: ')
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def number_sentences(t):
+    k = t.count('.')
+    k += t.count('!')
+    k += t.count('?')
+    return k
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def number_words(w):
+    w = w.split()
+    f = len(w)
+    return f
+
+
+def number_syllables(word):
+    vowels = 0
+    for let in word:
+        if let.isalpha():
+            if let.lower() in 'aeiouy':
+                vowels += 1
+            elif let.lower() in 'ауоыиэяюёе':
+                vowels += 1
+    return vowels
+
+
+def average_indicators(sentences, words, syllables):
+    asl = words / sentences
+    asw = syllables / words
+    return asl, asw
+
+
+sent1 = number_sentences(text)
+words1 = number_words(text)
+syllables1 = number_syllables(text)
+asl1, asw2 = average_indicators(sent1, words1, syllables1)
+
+print('Предложений:', sent1)
+print('Слов:', words1)
+print('Слогов:', syllables1)
+print('Средняя длина предложения в словах:', asl1)
+print('Средняя длина слова в слогах:', asw2)
+
