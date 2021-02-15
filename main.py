c227933e0da903794d31f4d2dbc045374c5409d8
memareve <mevdishe@gmail.com>
2021-02-15 13:09:58 +0700

    beta 0.2 add analysis (problem with polarity and objectivity in ru

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from textblob import TextBlob
 
+
 text = input('Введите текст: ')
 text_blob = TextBlob(text)
+detect_language = text_blob.detect_language()
 
 
 def number_sentences(t):
@@ -34,51 +36,63 @@ def average_indicators(sentences, words, syllables):
     return asl, asw
 
 
+def flash(asl2, asw2):
+    fre = -1
+    if detect_language == 'ru':
+        fre = 206.835 - (1.3 * asl2) - (60.1 * asw2)
+    elif detect_language == 'en':
+        fre = 206.835 - 1.015 * asl2 - 84.6 * asw2
+    return fre
+
+
+def flash_detect(fre2):
+    if detect_language == 'ru':
+        if fre2 > 80:
+            print('Текст очень легко читается (для младших школьников).')
+        elif fre2 > 50:
+            print('Простой текст (для школьников).')
+        elif fre2 > 25:
+            print('Текст немного трудно читать (для студентов).')
+        elif fre2 <= 25:
+            print('Текст трудно читается (для выпускников ВУЗов).')
+    elif detect_language == 'en':
+        if fre2 >= 100:
+            print('Очень легко читается.')
+        elif fre2 >= 65:
+            print('Простой английский язык.')
+        elif fre2 >= 30:
+            print('Немного трудно читать.')
+        elif fre2 < 30:
+            print('Очень трудно читать. ')
+
+
+def analysis(textb):
+    if detect_language == 'ru':
+        textb = text_blob.translate(to='en')
+    polarity = round(textb.polarity)
+    if polarity > 0:
+        polarity = 'позитивная'
+    elif polarity < 0:
+        polarity = 'негативная'
+    else:
+        polarity = 'нейтральная'
+    objectivity = (1 - textb.subjectivity) * 100
+    return polarity, objectivity
+
+
 sent1 = number_sentences(text)
 words1 = number_words(text)
 syllables1 = number_syllables(text)
 asl1, asw1 = average_indicators(sent1, words1, syllables1)
-
 print('Предложений:', sent1)
 print('Слов:', words1)
 print('Слогов:', syllables1)
 print('Средняя длина предложения в словах:', asl1)
 print('Средняя длина слова в слогах:', asw1)
-
-
-def flash(text1, asl2, asw2):
-    fre = -1
-    if text1.detect_language() == 'ru':
-        fre = 206.835 - (1.3 * asl2) - (60.1 * asw2)
-    elif text1.detect_language() == 'en':
-        fre = 206.835 - 1.015 * asl2 - 84.6 * asw2
-    return fre
-
-
-fre1 = flash(text_blob, asl1, asw1)
+fre1 = flash(asl1, asw1)
 print('Индекс удобочитаемости Флеша:', fre1)
-
-
-def flash_detect(textt, fre2):
-    if textt.detect_language() == 'ru':
-        if fre1 > 80:
-            print('Текст очень легко читается (для младших школьников).')
-        elif fre1 > 50:
-            print('Простой текст (для школьников).')
-        elif fre1 > 25:
-            print('Текст немного трудно читать (для студентов).')
-        elif fre1 <= 25:
-            print('Текст трудно читается (для выпускников ВУЗов).')
-    elif textt.detect_language() == 'en':
-        if fre1 >= 100:
-            print('Очень легко читается.')
-        elif fre1 >= 65:
-            print('Простой английский язык.')
-        elif fre1 >= 30:
-            print('Немного трудно читать.')
-        elif fre1 < 30:
-            print('Очень трудно читать. ')
-
-
-flash_detect(text_blob, fre1)
+flash_detect(fre1)
+polarit, objectiv = analysis(text_blob)
+print('Тональность текста:', polarit)
+print('Объективность: ', objectiv, '%', sep=' ')
 
